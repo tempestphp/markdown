c85dcaccdbe0dff484c053c41de8846984d63ed7
brendt <brent.roose@gmail.com>
2026-06-25 06:41:01 +0200

    wip

--- tests/Rules/ItalicRuleTest.php
+++ tests/Rules/ItalicRuleTest.php
@@ -5,6 +5,8 @@ namespace Tempest\Markdown\Tests\Rules;
 use PHPUnit\Framework\Attributes\Test;
 use Tempest\Markdown\Parser;
 use Tempest\Markdown\Rules\ItalicRule;
+use Tempest\Markdown\Rules\NewLineRule;
+use Tempest\Markdown\Rules\ParagraphRule;
 use Tempest\Markdown\Tests\ParserTestCase;
 
 class ItalicRuleTest extends ParserTestCase
@@ -15,6 +17,14 @@ class ItalicRuleTest extends ParserTestCase
         $html = (string) new Parser(highlighter: null, rules: [new ItalicRule()])->parse('_italic_');
 
         $this->assertSame('<em>italic</em>', $html);
+    }
+
+    #[Test]
+    public function test_underscore_must_be_terminated(): void
+    {
+        $html = (string) new Parser(highlighter: null, rules: [new NewLineRule(), new ItalicRule(), new ParagraphRule()])->parse("Hello_world\n\nHi");
+
+        $this->assertSame("<p>Hello_world</p>\n\n<p>Hi</p>", $html);
     }
 
     #[Test]
