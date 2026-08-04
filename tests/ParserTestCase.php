7a6e1b492ff659ac9fdf9befc0caca5486e077b4
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-08-04 18:27:14 +0200

    test: cleanup ParserTestCase

--- tests/ParserTestCase.php
+++ tests/ParserTestCase.php
@@ -2,15 +2,6 @@
 
 namespace Tempest\Markdown\Tests;
 
-use PHPUnit\Framework\Attributes\Before;
 use PHPUnit\Framework\TestCase;
-use Tempest\Markdown\Parser;
 
-abstract class ParserTestCase extends TestCase
-{
-    #[Before]
-    public function resetParser(): void
-    {
-        Parser::reset();
-    }
-}
+abstract class ParserTestCase extends TestCase {}
